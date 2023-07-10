b1d5a3730584b5346d6061ae0bf2de1be0b7dc82
karem-khater <124695737+karem-khater@users.noreply.github.com>
2023-07-10 21:26:42 +0300

    pushbutton task.ino

--- pushbutton task.ino
+++ pushbutton task.ino
@@ -1,17 +1,16 @@
 int led = 6;
-int button=5;
-int Reading=0;
-void setup()
-{
+int button = 5;
+int reading = LOW;
+
+void setup() {
   pinMode(led, OUTPUT);
   pinMode(button, INPUT);
 }
 
-void loop()
-{
-Reading=digitalRead(button);
-  if(Reading==HIGH){
-    digitalWrite(led,HIGH);
-    delay(5000);}
-  else{digitalWrite(led,LOW);}
+void loop() {
+  if (digitalRead(button) == HIGH) {
+    reading = !reading;
+    digitalWrite(led, reading);
+    delay(500);
+  }
 }
